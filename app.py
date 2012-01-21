1a4af97d73e9e270242508d760107adc6697443e
Jeff Balogh <me@jeffbalogh.org>
2012-01-20 17:05:39 -0800

    an app to send notifications

---
+++ app.py
@@ -0,0 +1,38 @@
+import requests
+
+from flask import Flask, request
+app = Flask(__name__)
+
+db = {}
+
+
+@app.route('/')
+def root():
+    return open('index.html').read()
+
+
+@app.route('/queue', methods=['POST'])
+def add_queue():
+    user = request.form['user']
+    queue = request.form['queue']
+    db[user] = queue
+    print db
+    return ''
+
+
+@app.route('/message', methods=['POST'])
+def send_message():
+    name = request.form['user']
+    message = request.form['message']
+    for user, queue in db.items():
+        if name != user:
+            notify(queue, 'Message from %s' % name, message)
+    return ''
+
+
+def notify(queue, title, text):
+    print requests.post(queue, {'title': title, 'body': text})
+
+
+if __name__ == '__main__':
+    app.run(port=5002, debug=True)
